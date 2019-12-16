f03677500f987d73cbf2aae54800d5c41074cbe5
Karoly Nyisztor <ny-karoly@t-online.hu>
2019-12-16 10:39:03 +0100

    Replaced arc4random_uniform() with Array shuffled() in generateRandom() call

--- algorithm-power-src/FindTwoSum.playground/Sources/ArrayExtension.swift
+++ algorithm-power-src/FindTwoSum.playground/Sources/ArrayExtension.swift
@@ -2,12 +2,9 @@ import Foundation
 
 public extension Array where Element == Int {
     static func generateRandom(size: Int, maxValue: Int) -> [Int] {
-        var result = [Int](repeating: 0, count:size)
-        
-        for i in 0..<size {
-            result[i] = Int(arc4random_uniform(UInt32(maxValue)))
+        guard size > 0 else {
+            return [Int]()
         }
-        
-        return result
+        return Array(0..<size).shuffled()
     }
 }
