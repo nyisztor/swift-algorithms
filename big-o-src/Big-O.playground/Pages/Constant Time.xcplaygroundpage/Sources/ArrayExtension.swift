fbf518407d0915f991645a1c76d92b6091b0291d
Karoly Nyisztor <nyisztorkaroly@gmail.com>
2022-07-04 11:59:49 +0200

    Using array isEmpty instead of checking item count

--- big-o-src/Big-O.playground/Pages/Constant Time.xcplaygroundpage/Sources/ArrayExtension.swift
+++ big-o-src/Big-O.playground/Pages/Constant Time.xcplaygroundpage/Sources/ArrayExtension.swift
@@ -3,7 +3,7 @@ import Foundation
 public extension Array where Element == Int {
     // Checks whether the first element of the array is zero
     func startsWithZero() -> Bool {
-        guard self.count != 0 else {
+        guard self.isEmpty == false else {
             return false
         }
         return self.first == 0 ? true : false
